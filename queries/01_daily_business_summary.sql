5fe69abd322d12cd06f241a513ea7504944f216a
snitin378-spec <s.nitin378@gmail.com>
2026-07-24 13:59:09 +0530

    Enhance daily business summary with refund amount
    Added refund amount calculation to daily business summary.

--- queries/01_daily_business_summary.sql
+++ queries/01_daily_business_summary.sql
@@ -1,14 +1,29 @@
 WITH daily_summary AS
 (
     SELECT
-        DATE(created_at) AS order_date,
-        COUNT(order_id) AS total_orders,
-        SUM(subtotal) AS revenue,
-        ROUND(SUM(subtotal) * 1.0 / COUNT(order_id), 2) AS aov,
-        COUNT(*) FILTER (WHERE LOWER(status) = 'paid') AS paid_orders,
-        COUNT(*) FILTER (WHERE LOWER(status) = 'cancelled') AS cancelled_orders
-    FROM ecom.orders
-    GROUP BY DATE(created_at)
+        DATE(o.created_at) AS order_date,
+        COUNT(o.order_id) AS total_orders,
+        SUM(o.subtotal) AS revenue,
+        ROUND(SUM(o.subtotal) * 1.0 / COUNT(o.order_id),2) AS aov,
+
+        COUNT(*) FILTER
+        (
+            WHERE LOWER(o.status)='paid'
+        ) AS paid_orders,
+
+        COUNT(*) FILTER
+        (
+            WHERE LOWER(o.status)='cancelled'
+        ) AS cancelled_orders,
+
+        SUM(COALESCE(r.amount,0)) AS refund_amount
+
+    FROM ecom.orders o
+
+    LEFT JOIN ecom.refunds r
+        ON o.order_id = r.order_id
+
+    GROUP BY DATE(o.created_at)
 )
 
 SELECT
@@ -18,7 +33,21 @@ SELECT
     aov,
     paid_orders,
     cancelled_orders,
-    LAG(revenue) OVER (ORDER BY order_date) AS yesterday_revenue,
-    revenue - LAG(revenue) OVER (ORDER BY order_date) AS revenue_difference
+    refund_amount,
+
+    LAG(revenue)
+    OVER
+    (
+        ORDER BY order_date
+    ) AS yesterday_revenue,
+
+    revenue -
+    LAG(revenue)
+    OVER
+    (
+        ORDER BY order_date
+    ) AS revenue_difference
+
 FROM daily_summary
+
 ORDER BY order_date;
